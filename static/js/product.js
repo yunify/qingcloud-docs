ec1975caeb148f77ad15dd75d9bc6f2707542bcc
yuancong@yunify.com <19890912Cc!>
2020-12-15 11:42:10 +0800

    跳转js bugfix

--- static/js/product.js
+++ static/js/product.js
@@ -1,8 +1,8 @@
 $(function(){
 	//查看链接结尾是否/结尾
 	$('a').each(function(){
+		var url = $(this).attr('href');
 		if(url != null && url != 'undefined'){
-			var url = $(this).attr('href');
 			var start = url.length-1;
 			var last = url.substr(start,1);
 			if(last != '/'){
