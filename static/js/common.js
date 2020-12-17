76bf8e8b6ed94eaaec073d3c826604d797184af7
yuancong@yunify.com <19890912Cc!>
2020-12-17 11:45:01 +0800

    console链接问题修复

--- static/js/common.js
+++ static/js/common.js
@@ -32,8 +32,7 @@ $(function(){
 	//查看链接结尾是否/结尾
 	$('a').each(function(){
 		var url = $(this).attr('href');
-		if(url != null && url != 'undefined' && url.indexOf('http') != '-1' && url.indexOf('#') != '-1'){
-			console.log(url);
+		if(url != null && url != 'undefined' && url.indexOf('http') == '-1' && url.indexOf('#') == '-1'){
 			var start = url.length-1;
 			var last = url.substr(start,1);
 			if(last != '/'){
