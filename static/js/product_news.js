ce5870df7b22952beac3b908e1c84ad0159f57b4
wuxiaoxue <wuxiaoxue@innobuddy.com>
2021-11-18 11:03:55 +0800

    fix: 修复：【问题】产品动态tags在本页目录的标题中不应该显示出来

--- static/js/product_news.js
+++ static/js/product_news.js
@@ -1,16 +1,19 @@
 //产品动态无法自动生成toc目录列表 特用js生成
-$(function(){
-	if($('#TableOfContents li').length == 0){
-		var h2_title = $('section.product_dynamics h2');
-		if(h2_title.length >0){
-			var result = '<ul>';
-			h2_title.each(function(){
-				var h2_id = $(this).attr('id');
-				var h2_name = $(this).html();
-				result += '<li><a href="#'+h2_id+'">'+h2_name+'</a></li>';
-			});
-			result += '</ul>';
-			$('#TableOfContents').html(result);
-		}
-	}
-})
+$(function () {
+  if ($("#TableOfContents li").length == 0) {
+    var h2_title = $("section.product_dynamics h2");
+    console.log("h2_title: ", h2_title);
+    if (h2_title.length > 0) {
+      var result = "<ul>";
+      h2_title.each(function () {
+        var h2_id = $(this).attr("id");
+        console.log("h2_id: ", h2_id);
+        // var h2_name = $(this).html();
+        // console.log("h2_name: ", h2_name);
+        result += '<li><a href="#' + h2_id + '">' + h2_id + "</a></li>";
+      });
+      result += "</ul>";
+      $("#TableOfContents").html(result);
+    }
+  }
+});
