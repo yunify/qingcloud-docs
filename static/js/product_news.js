2047f63e6d4c880c2762dde5aeb25d7fa4b69ff8
Wciel <1442434160@qq.com>
2021-11-26 10:48:48 +0800

    Fixbug/添加sitemap模板，产品动态右边目录栏改成时间轴 (#623)
    * fix: 修复：【问题】产品动态tags在本页目录的标题中不应该显示出来
    
    * fix: 修复DC-408、DC-377、DC-376、DC-395相关jira问题,
    
    * fix: 添加table左侧虚线
    
    * fix: 添加sitemap模板，产品动态右边目录栏改成时间轴
    
    Co-authored-by: wuxiaoxue <wuxiaoxue@innobuddy.com>

--- static/js/product_news.js
+++ static/js/product_news.js
@@ -1,19 +1,50 @@
 //产品动态无法自动生成toc目录列表 特用js生成
 $(function () {
-  if ($("#TableOfContents li").length == 0) {
-    var h2_title = $("section.product_dynamics h2");
-    console.log("h2_title: ", h2_title);
-    if (h2_title.length > 0) {
-      var result = "<ul>";
-      h2_title.each(function () {
-        var h2_id = $(this).attr("id");
-        console.log("h2_id: ", h2_id);
-        // var h2_name = $(this).html();
-        // console.log("h2_name: ", h2_name);
-        result += '<li><a href="#' + h2_id + '">' + h2_id + "</a></li>";
-      });
-      result += "</ul>";
-      $("#TableOfContents").html(result);
+  const TopPageTitle = $(".td-page-top h1");
+  //产品动态时间轴化
+  if (TopPageTitle[0]?.innerHTML?.includes("产品动态")) {
+    if ($("#TableOfContents li").length == 0) {
+      var time_line = $("section.product_dynamics .timeline-outer");
+      var h2_title = $("section.product_dynamics h2");
+      if (time_line.length > 0) {
+        var result = "<ul>";
+        time_line.each(function (index) {
+          var h2_id = $(h2_title[index]).attr("id");
+          var time_p = $(this).find("p");
+          const time = timeStamp2String(time_p[0]?.innerHTML);
+          result += '<li><a href="#' + h2_id + '">' + time + "</a></li>";
+        });
+        result += "</ul>";
+        $("#TableOfContents").html(result);
+      }
     }
+  } else {
+    // 非产品动态目录化
+    if ($("#TableOfContents li").length == 0) {
+      const h2_title1 = $("section.product_dynamics");
+      var h2_title = $("section.product_dynamics h2");
+      if (h2_title.length > 0) {
+        var result = "<ul>";
+        h2_title.each(function () {
+          var h2_id = $(this).attr("id");
+
+          result += '<li><a href="#' + h2_id + '">' + h2_id + "</a></li>";
+        });
+        result += "</ul>";
+        $("#TableOfContents").html(result);
+      }
+    }
+  }
+
+  function timeStamp2String(time) {
+    const datetime = new Date(time);
+    const year = datetime.getFullYear();
+    const month =
+      datetime.getMonth() + 1 < 10
+        ? "0" + (datetime.getMonth() + 1)
+        : datetime.getMonth() + 1;
+    const date =
+      datetime.getDate() < 10 ? "0" + datetime.getDate() : datetime.getDate();
+    return `<span style="display:inline-block; text-align: center;width: 32px">${year}</span>   年   <span style="display:inline-block; text-align: center;width: 14px">${month}</span>  月  <span style="display:inline-block; text-align: center; width: 14px">${date}</span>  日`;
   }
 });
